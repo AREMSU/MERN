27b4d2c89de255d774749ced3317e05a8443c24d
AREMSU <arnavjungdahala1@gmail.com>
2026-09-20 11:29:46 +0545

    frontend setup for integration

--- integration/Frontend/src/product/CreateProduct.jsx
+++ integration/Frontend/src/product/CreateProduct.jsx
@@ -21,6 +21,42 @@ const CreateProduct = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <div>
+          <label>Product Name</label>
+          <input type="text"
+            onChange={(e) => {
+              setName(e.target.value)
+            }
+            }
+            value={name}></input>
+        </div>
+        <div>
+          <label>Price</label>
+          <input type="number"
+            onChange={(e) => {
+              setPrice(e.target.value)
+            }
+            }
+            value={price}></input>
+        </div>
+        <div>
+          <label>Quantity</label>
+          <input type="number"
+            onChange={(e) => {
+              setQuantity(e.target.value)
+            }
+            }
+            value={quantity}></input>
+        </div>
+        <div>
+          <label>Description</label>
+          <textarea
+            onChange={(e) => {
+              setDescription(e.target.value)
+            }
+            }
+            value={description}></textarea>
+        </div>
         <button>Submit</button>
       </form>
     </div>
--- integration/Frontend/src/user/CreateUser.jsx
+++ integration/Frontend/src/user/CreateUser.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateUser = () => {
+  let [name, setName] = useState("")
+  let [email, setEmail] = useState("")
+  let [password, setPassword] = useState("")
+  let [address, setAddress] = useState("")
+  let [phone, setPhone] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    let data = {
+      name: name,
+      email: email,
+      password: password,
+      address: address,
+      phone: phone,
+    }
+    console.log(data)
+  }
   return (
-    <div>CreateUser</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>User Name</label>
+          <input type="text"
+            onChange={(e) => {
+              setName(e.target.value)
+            }
+            }
+            value={name}></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email"
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }
+            }
+            value={email}></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }
+            }
+            value={password}></input>
+        </div>
+        <div>
+          <label>Address</label>
+          <input type="text"
+            onChange={(e) => {
+              setAddress(e.target.value)
+            }
+            }
+            value={address}></input>
+        </div>
+        <div>
+          <label>Phone</label>
+          <input type="number"
+            onChange={(e) => {
+              setPhone(e.target.value)
+            }
+            }
+            value={phone}></input>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
-
 export default CreateUser
